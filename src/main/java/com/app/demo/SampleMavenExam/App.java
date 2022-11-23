3a1574ad0e8b175dbd3e0fdffe5d18abf708bbc7
SathvikRakonda <sathvik.rakonda@gmail.com>
2022-11-23 11:31:03 +0530

    internal

---
+++ src/main/java/com/app/demo/SampleMavenExam/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.SampleMavenExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE Lab Internal 1 exam" );
+    }
+}
